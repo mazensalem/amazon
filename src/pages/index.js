613c2d10916219b6215d8cdd320603434bb26968
mazensalem <salemmazen27@gmail.com>
2021-05-20 22:27:17 +0200

    The Banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,22 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
+import ProductsList from "../components/ProductsList";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
       {/* Header */}
       <Header />
-      {/* banar */}
-      {/* products */}
+      <main className="max-w-screen-2xl mx-auto">
+        {/* banner */}
+        <Banner />
+        {/* products */}
+        <ProductsList />
+      </main>
     </div>
   );
 }
